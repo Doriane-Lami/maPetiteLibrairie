fd66d94360b397125125f1499868b526e711b2d7
Doriane Lami <doriane.lami@etud.univ-jfc.fr>
2023-02-15 20:33:35 +0000

    tableau des stocks pas de gestion de qté

--- src/components/LivreItem.vue
+++ src/components/LivreItem.vue
@@ -1,10 +1,12 @@
 <script setup>
 defineProps(["livre"]);
+const emit = defineEmits(["deleteL"]);
 </script>
 
 <template>
   <li>
     {{ livre.pourAfficher() }}
+    <button @click="$emit('deleteL', livre.id)">Supprimer</button>
   </li>
 </template>
 
